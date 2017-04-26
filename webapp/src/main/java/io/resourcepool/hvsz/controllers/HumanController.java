82b0bbebf557da6063e4af0102d43d5f9fa83216
gdanguy <gdanguy@e-biz.fr>
2017-04-26 16:35:44 +0200

    [Feature] Add HumanController and ResourceController

--- webapp/src/main/java/io/resourcepool/hvsz/controllers/HumanController.java
+++ webapp/src/main/java/io/resourcepool/hvsz/controllers/HumanController.java
@@ -1,16 +1,19 @@
 package io.resourcepool.hvsz.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class DashboardController {
-  @GetMapping("/dashboard")
-  public String dashboard(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-    model.addAttribute("name", name);
-    return "dashboard";
+public class HumanController {
+  @GetMapping("/human")
+  public String human(@RequestParam(value="newlife", required=false) String newLife, Model model) {
+    if(newLife != null) {
+      model.addAttribute("newlife", "New life for you <3");
+    }
+    return "human";
   }
 
 }
