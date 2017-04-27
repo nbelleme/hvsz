0d7778f8d8004ff10df13e3a4739e8c4c47b8abe
Logan Lepage <llepage@e-biz.fr>
2017-04-27 18:04:45 +0200

    [feature:status] Add status controller to start the game

--- webapp/src/main/java/io/resourcepool/hvsz/service/ConfigurationService.java
+++ webapp/src/main/java/io/resourcepool/hvsz/service/ConfigurationService.java
@@ -3,5 +3,7 @@ package io.resourcepool.hvsz.service;
 import io.resourcepool.hvsz.persistance.models.GameConfig;
 
 public interface  ConfigurationService {
+
     GameConfig add(GameConfig gameConfig, Long gameId);
+    GameConfig get(Long gameId);
 }
