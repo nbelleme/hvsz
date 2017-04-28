1eff2644d987b670124fee6c650bb824a23d4a3b
gdanguy <gdanguy@e-biz.fr>
2017-04-28 16:03:28 +0200

    [Debut de Feature] Add select zone to display

--- webapp/src/main/java/io/resourcepool/hvsz/service/ResourceService.java
+++ webapp/src/main/java/io/resourcepool/hvsz/service/ResourceService.java
@@ -2,6 +2,9 @@ package io.resourcepool.hvsz.service;
 
 import io.resourcepool.hvsz.persistance.models.SafeZone;
 import io.resourcepool.hvsz.persistance.models.SupplyZone;
+import io.resourcepool.hvsz.persistance.models.ZoneResource;
+
+import java.util.List;
 
 public interface ResourceService {
 
@@ -14,6 +17,14 @@ public interface ResourceService {
     int get(SupplyZone supplyZone, int amount);
 
     /**
+     * Take resources on a supply zone.
+     * @param idSupplyZone the id of the supply zone where we take resources.
+     * @param amount number max of resources we want to take.
+     * @return the number of resources taken.
+     */
+    int get(int idSupplyZone, int amount);
+
+    /**
      * Drop resources on a safe zone.
      * @param safeZone the safe zone where we drop resources.
      * @param amount number max of resources we want to drop.
@@ -22,10 +33,44 @@ public interface ResourceService {
     int drop(SafeZone safeZone, int amount);
 
     /**
+     * Take resources on a supply zone.
+     * @param idSafeZone the id of the supply zone where we drop resources.
+     * @param amount number max of resources we want to drop.
+     * @return the new status of the SafeZone.
+     */
+    int drop(int idSafeZone, int amount);
+
+    /**
      * Decrease an amount on all safezones.
      * @param amount of resource de decrease all Safe Zone
      */
     void decreaseSafezones(int amount);
+
+    /**
+     * Get the correponding safe zone.
+     * @param safeId the id of the safe zone
+     * @return the SafeZone
+     */
+    SafeZone getSafeZone(String safeId);
+
+    /**
+     * Get the correponding safe zone.
+     * @param safeId the id of the safe zone
+     * @return the SafeZone
+     */
+    SafeZone getSafeZone(int safeId);
+
+    /**
+     * Return all the Safe Zone.
+     * @return a list of safe zone
+     */
+    List<SafeZone> getAllSafeZone();
+
+    /**
+     * Return all the Safe Zone and Supply Zone.
+     * @return a list of ZoneResource
+     */
+    List<ZoneResource> getAllZoneResource();
 
     /**
      * Drop resources on a safe zone by id.
