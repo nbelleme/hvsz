74dc4f6e94586a07a5e60b0c9abe82c51e7379ae
Logan Lepage <llepage@e-biz.fr>
2017-04-28 14:50:54 +0200

    [refactor:resource] Prepare resource controller to next features

--- webapp/src/main/java/io/resourcepool/hvsz/controllers/ResourceController.java
+++ webapp/src/main/java/io/resourcepool/hvsz/controllers/ResourceController.java
@@ -22,35 +22,51 @@ public class ResourceController {
     /**
      * Drop a resource.
      * @param safeZone SupplyZone id
+     * @param lifeId life id
      * @param model Model
      * @return String (human vue)
      */
     @PostMapping("/resource/drop")
-    public String dropResource(@RequestParam(value = "safeZone", required = false) String safeZone, Model model) {
-        if (safeZone != null) {
-            Game g = dao.get(1L);
-            SafeZone s = g.getSafeZones().get(Integer.parseInt(safeZone));
-            model.addAttribute("nbResources", s.drop(1) + " resource has been dropped : supply zone n°" + ID_SUPPLY_ZONE + " contains :" + s.getResource() + "resources");
-            dao.set(1L, g);
-        }
+    public String dropResource(
+            @RequestParam(value = "safeZone") String safeZone,
+            @RequestParam(value = "lifeId") String lifeId,
+            Model model) {
+
+        //TODO: Faire une méthode pour dropper des ressources par un utilisateur (lifeId)
+        Game g = dao.get(1L);
+        SafeZone s = g.getSafeZones().get(Integer.parseInt(safeZone));
+        model.addAttribute("nbResources", s.drop(1) + " resource has been dropped : supply zone n°" + ID_SUPPLY_ZONE + " contains :" + s.getResource() + "resources");
+        dao.set(1L, g);
         return "human";
     }
 
     /**
-     * Take a resource.
-     * @param supplyZone SupplyZone id
-     * @param model Model
+     * Show the supply-zone view
      * @return String (supply-zone vue)
      */
     @GetMapping("/resource/get")
-    public String resourceGet(@RequestParam(value = "supplyZone", required = false) String supplyZone, Model model) {
+    public String resourceGet() {
+        return "supply-zone";
+    }
 
-        if (supplyZone != null) {
-            Game g = dao.get(1L);
-            SupplyZone s = g.getSupplyZones().get(Integer.parseInt(supplyZone));
-            model.addAttribute("nbSupplyResources", s.getResource(1) + " resource has been taken : remaining resources :" + s.getResource());
-            dao.set(1L, g);
-        }
+    /**
+     * Take a resource.
+     * @param supplyZone Supply zone id
+     * @param lifeId life id
+     * @param model Model
+     * @return String (supply-zone)
+     */
+    @PostMapping("/resource/get")
+    public String resourceGet(
+            @RequestParam(value = "supplyZone") String supplyZone,
+            @RequestParam(value = "lifeId") String lifeId,
+            Model model) {
+
+        //TODO: prendre une ressource en tant qu'utilisateur (lifeId) dans un supplyZone
+        Game g = dao.get(1L);
+        SupplyZone s = g.getSupplyZones().get(Integer.parseInt(supplyZone));
+        model.addAttribute("nbSupplyResources", s.getResource(1) + " resource has been taken : remaining resources :" + s.getResource());
+        dao.set(1L, g);
         return "supply-zone";
     }
 }
