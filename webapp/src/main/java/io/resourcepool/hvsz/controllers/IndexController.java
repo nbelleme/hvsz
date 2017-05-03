2c976a5cfc8ac3dca79b3e6235f52edf06515f38
gdanguy <gdanguy@e-biz.fr>
2017-05-03 14:39:02 +0200

    [Refactoring] update link of index

--- webapp/src/main/java/io/resourcepool/hvsz/controllers/IndexController.java
+++ webapp/src/main/java/io/resourcepool/hvsz/controllers/IndexController.java
@@ -1,14 +1,10 @@
 package io.resourcepool.hvsz.controllers;
 
-import io.resourcepool.hvsz.service.StatusService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-  @Autowired
-  private StatusService statusService;
 
   /**
    * Get the index page.
