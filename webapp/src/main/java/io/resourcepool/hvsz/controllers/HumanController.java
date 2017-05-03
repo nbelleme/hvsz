933fe87aff5a2df69139f21faff13a858fe9fd1f
Logan Lepage <llepage@e-biz.fr>
2017-05-03 14:13:33 +0200

    [refactor:zones] Zones now have an action controller

--- webapp/src/main/java/io/resourcepool/hvsz/controllers/HumanController.java
+++ webapp/src/main/java/io/resourcepool/hvsz/controllers/HumanController.java
@@ -2,7 +2,6 @@ package io.resourcepool.hvsz.controllers;
 
 import io.resourcepool.hvsz.persistance.models.GameStateEnum;
 import io.resourcepool.hvsz.persistance.models.Life;
-import io.resourcepool.hvsz.persistance.models.Zone;
 import io.resourcepool.hvsz.persistance.models.ZoneResource;
 import io.resourcepool.hvsz.service.HumanService;
 import io.resourcepool.hvsz.service.ResourceService;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -19,90 +17,59 @@ import java.util.List;
 @Controller
 public class HumanController {
 
-  @Autowired
-  HumanService humanService;
-  @Autowired
-  ResourceService resourceService;
-  @Autowired
-  StatusService statusService;
+    @Autowired
+    HumanService humanService;
+    @Autowired
+    ResourceService resourceService;
+    @Autowired
+    StatusService statusService;
 
-  /**
-   * Get the human page.
-   *
-   * @param newLife String
-   * @param model   Model
-   * @return String (human)
-   */
-  @GetMapping("/human")
-  public String human(@RequestParam(value = "newlife", required = false) String newLife, Model model) {
-    if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
-      return "redirect:/game/over";
-    }
-
-    if (newLife != null) {
-      Life life = humanService.newLife();
-      String lifeToken = null;
-      if (life != null) {
-        lifeToken = life.getToken();
-      }
-      if (lifeToken != null) {
-        model.addAttribute("newlife", "Une nouvelle vie pour toi <3  token: " + lifeToken);
-      } else {
-        if (humanService.countLifeLeft() > 0) {
-          model.addAttribute("newlife", "Toutes les vies sont utilisées");
-        } else {
-          model.addAttribute("newlife", "Toutes les vies sont en cours d'utilisation (les zombies ont un petit appétit) ;-(");
+    /**
+     * Get the human page.
+     *
+     * @param newLife String
+     * @param model   Model
+     * @return String (human)
+     */
+    @GetMapping("/human")
+    public String human(@RequestParam(value = "newlife", required = false) String newLife, Model model) {
+        if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
+            return "redirect:/game/over";
         }
-      }
-    }
-    return "human";
-  }
 
-  /**
-   * Interface for select a safe zone.
-   *
-   * @param model Model
-   * @return String (safe-zone vue)
-   */
-  @GetMapping("/human/zone")
-  public String selectZone(Model model) {
-    if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
-      return "redirect:/game/over";
-    }
-
-    List<ZoneResource> zones = resourceService.getAllZoneResource();
-    model.addAttribute("zones", zones);
-    return "zone";
-  }
-
-  /**
-   * Interface for select a safe zone.
-   *
-   * @param model Model
-   * @param id    the zone we want to access
-   * @param type  the type of zone
-   * @return String (safe-zone vue)
-   */
-  @PostMapping("/human/zone")
-  public String displayZone(
-      @RequestParam(value = "id") String id,
-      @RequestParam(value = "type") String type,
-      Model model) {
-    if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
-      return "redirect:/game/over";
+        if (newLife != null) {
+            Life life = humanService.newLife();
+            String lifeToken = null;
+            if (life != null) {
+                lifeToken = life.getToken();
+            }
+            if (lifeToken != null) {
+                model.addAttribute("newlife", "Une nouvelle vie pour toi <3  token: " + lifeToken);
+            } else {
+                if (humanService.countLifeLeft() > 0) {
+                    model.addAttribute("newlife", "Toutes les vies sont utilisées");
+                } else {
+                    model.addAttribute("newlife", "Toutes les vies sont en cours d'utilisation (les zombies ont un petit appétit) ;-(");
+                }
+            }
+        }
+        return "human";
     }
 
-    Zone zone;
-    if (type.equals("safezone")) {
-      zone = resourceService.getSafeZone(Integer.parseInt(id));
-      model.addAttribute("zone", zone);
-      model.addAttribute("game_status", statusService.get(1L).getGameState());
-      return "safe-zone";
-    } else {
-      zone = resourceService.getSupplyZone(Integer.parseInt(id));
-      model.addAttribute("maxResource", Life.getMaxResources());
-      model.addAttribute("zone", zone);
-      return "supply-zone";
+    /**
+     * Interface for select a safe zone.
+     *
+     * @param model Model
+     * @return String (safe-zone vue)
+     */
+    @GetMapping("/human/zone")
+    public String selectZone(Model model) {
+        if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
+            return "redirect:/game/over";
+        }
+
+        List<ZoneResource> zones = resourceService.getAllZoneResource();
+        model.addAttribute("zones", zones);
+        return "zone";
     }
-  }
 }
--- webapp/src/main/java/io/resourcepool/hvsz/controllers/ResourceController.java
+++ webapp/src/main/java/io/resourcepool/hvsz/controllers/ResourceController.java
@@ -6,6 +6,7 @@ import io.resourcepool.hvsz.persistance.models.GameStateEnum;
 import io.resourcepool.hvsz.persistance.models.Life;
 import io.resourcepool.hvsz.persistance.models.SafeZone;
 import io.resourcepool.hvsz.persistance.models.SupplyZone;
+import io.resourcepool.hvsz.persistance.models.Zone;
 import io.resourcepool.hvsz.service.HumanService;
 import io.resourcepool.hvsz.service.ResourceService;
 import io.resourcepool.hvsz.service.StatusService;
@@ -34,52 +35,83 @@ public class ResourceController {
     private static final int ID_SUPPLY_ZONE = 1;
 
     /**
+     * Show the safe-zone view.
+     *
+     * @param zoneId zoneId
+     * @param model  Model
+     * @return String (safe-zone vue)
+     */
+    @GetMapping("/resource/drop")
+    public String resourceDrop(
+            @RequestParam(value = "zoneId") String zoneId,
+            Model model) {
+        if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
+            return "redirect:/game/over";
+        }
+        
+        Zone zone = resourceService.getSafeZone(Integer.parseInt(zoneId));
+        model.addAttribute("zone", zone);
+        return "safe-zone";
+    }
+
+    /**
      * Drop a resource.
      *
-     * @param safeZone  SupplyZone id
+     * @param zoneId    SupplyZone id
      * @param lifeToken lifeToken
      * @param model     Model
      * @return String (human vue)
      */
     @PostMapping("/resource/drop")
     public String dropResource(
-            @RequestParam(value = "safeZone") String safeZone,
+            @RequestParam(value = "zoneId") String zoneId,
             @RequestParam(value = "lifeToken") String lifeToken,
             Model model) {
         if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
             return "redirect:/game/over";
         }
 
-    Game g = dao.get(1L);
-      if (g.getStatus().getLifeByToken(lifeToken) == null) {
-          model.addAttribute("message", "Wrong token, please try again !");
-      } else if (!g.getStatus().getLifeByToken(lifeToken).isAlive()) {
-        model.addAttribute("message", "You are dead !");
-      } else {
-          int lifeId = g.getStatus().getLifeByToken(lifeToken).getId();
-          int nbRes = humanService.getLife(lifeId).getNbResources();
-          int dropRes = resourceService.dropById(Integer.parseInt(safeZone), nbRes, lifeId);
-          model.addAttribute("nbDropped", dropRes);
-      }
-      SafeZone s = g.getSafeZoneById(Integer.parseInt(safeZone));
-      model.addAttribute("zone", s);
-      return "safe-zone";
-  }
+        Game g = dao.get(1L);
+        if (g.getStatus().getLifeByToken(lifeToken) == null) {
+            model.addAttribute("message", "Token invalide, essaye encore !");
+        } else if (!g.getStatus().getLifeByToken(lifeToken).isAlive()) {
+            model.addAttribute("message", "Tu es mort !");
+        } else {
+            int lifeId = g.getStatus().getLifeByToken(lifeToken).getId();
+            int nbRes = humanService.getLife(lifeId).getNbResources();
+            int dropRes = resourceService.dropById(Integer.parseInt(zoneId), nbRes, lifeId);
+            model.addAttribute("nbDropped", dropRes);
+        }
+        SafeZone s = g.getSafeZoneById(Integer.parseInt(zoneId));
+        model.addAttribute("zone", s);
+        return "safe-zone";
+    }
 
     /**
      * Show the supply-zone view.
      *
+     * @param zoneId zoneId
+     * @param model  Model
      * @return String (supply-zone vue)
      */
-    @GetMapping("/resource")
-    public String resourceGet() {
+    @GetMapping("/resource/get")
+    public String resourceGet(
+            @RequestParam(value = "zoneId") String zoneId,
+            Model model) {
+        if (!statusService.get(1L).getGameState().equals(GameStateEnum.ONGOING.name())) {
+            return "redirect:/game/over";
+        }
+
+        Zone zone = resourceService.getSupplyZone(Integer.parseInt(zoneId));
+        model.addAttribute("maxResource", Life.getMaxResources());
+        model.addAttribute("zone", zone);
         return "supply-zone";
     }
 
     /**
      * Take a resource.
      *
-     * @param supplyZone  SupplyZone id
+     * @param zoneId      SupplyZone id
      * @param nbResWanted number of resource wanted
      * @param lifeToken   lifeToken
      * @param model       Model
@@ -87,7 +119,7 @@ public class ResourceController {
      */
     @PostMapping("/resource/get")
     public String resourceGet(
-            @RequestParam(value = "supplyZone") String supplyZone,
+            @RequestParam(value = "zoneId") String zoneId,
             @RequestParam(value = "nbResWanted") String nbResWanted,
             @RequestParam(value = "lifeToken") String lifeToken,
             Model model) {
@@ -95,20 +127,21 @@ public class ResourceController {
             return "redirect:/game/over";
         }
 
-    Game g = dao.get(1L);
-    Life lifeId = g.getStatus().getLifeByToken(lifeToken);
-      if (g.getStatus().getLifeByToken(lifeToken) == null) {
-          model.addAttribute("message", "Wrong token, please try again !");
-      } else if (!g.getStatus().getLifeByToken(lifeToken).isAlive()) {
-        model.addAttribute("message", "You are dead !");
-      } else {
-          int id = lifeId.getId();
-          int gotRes = humanService.getResources(Integer.parseInt(supplyZone), Integer.parseInt(nbResWanted), id);
-          model.addAttribute("nbTaken", gotRes);
-      }
-      g = dao.get(1L);
-      SupplyZone s = g.getSupplyZoneById(Integer.parseInt(supplyZone));
-      model.addAttribute("zone", s);
-      return "supply-zone";
-  }
+        Game g = dao.get(1L);
+        Life lifeId = g.getStatus().getLifeByToken(lifeToken);
+        if (g.getStatus().getLifeByToken(lifeToken) == null) {
+            model.addAttribute("message", "Token invalide, essaye encore !");
+        } else if (!g.getStatus().getLifeByToken(lifeToken).isAlive()) {
+            model.addAttribute("message", "Tu es mort !");
+        } else {
+            int id = lifeId.getId();
+            int gotRes = humanService.getResources(Integer.parseInt(zoneId), Integer.parseInt(nbResWanted), id);
+            model.addAttribute("nbTaken", gotRes);
+        }
+        g = dao.get(1L);
+        SupplyZone s = g.getSupplyZoneById(Integer.parseInt(zoneId));
+        model.addAttribute("maxResource", Life.getMaxResources());
+        model.addAttribute("zone", s);
+        return "supply-zone";
+    }
 }
