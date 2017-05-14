70a4b02c6828d961375e3d14ee18ce6366c1ae88
nbelleme <nbelleme@e-biz.fr>
2017-05-14 04:13:19 +0200

    [angular][wip] food-supplies functions

--- hvsz-web/src/app/foodsupply/food-supply.service.ts
+++ hvsz-web/src/app/foodsupply/food-supply.service.ts
@@ -5,22 +5,21 @@ import {FoodSupply} from "./food-supply";
 
 @Injectable()
 export class FoodSuppplyService {
-    private foodSupplyUrl = 'http://localhost:8080/api/food-supply';  // URL to web api
+    private foodSupplyUrl = 'http://'+window.location.hostname+':8080/api/food-supply';  // URL to web api
     private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) {
 
     }
+
     getZone(id: number): Promise<FoodSupply> {
         const url = `${this.foodSupplyUrl}/${id}`;
         return this.http.get(url, {headers : this.headers})
             .toPromise()
-            .then(function (response) {
-                console.log(response.json());
-                return response.json() as FoodSupply
-            })
+            .then(response => response.json() as FoodSupply)
             .catch(this.handleError);
     }
+
     private handleError(error: any): Promise<any> {
         return Promise.reject(error.message || error);
     }
