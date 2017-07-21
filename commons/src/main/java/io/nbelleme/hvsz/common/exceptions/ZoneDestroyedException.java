7d6d169c1bd7293dae0c833a07039902b3a479e8
nbelleme <nbelleme@e-biz.fr>
2017-07-22 00:46:05 +0200

    [ NEW ] : add serialVerionUID

--- commons/src/main/java/io/nbelleme/hvsz/common/exceptions/ZoneDestroyedException.java
+++ commons/src/main/java/io/nbelleme/hvsz/common/exceptions/ZoneDestroyedException.java
@@ -1,4 +1,5 @@
 package io.nbelleme.hvsz.common.exceptions;
 
 public class ZoneDestroyedException extends IllegalStateException {
+  private static final long serialVersionUID = -4085331643326794460L;
 }
