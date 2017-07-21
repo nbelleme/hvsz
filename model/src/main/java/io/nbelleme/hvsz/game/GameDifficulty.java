3a737253de42df2d19a83399445382722766d784
nbelleme <nbelleme@e-biz.fr>
2017-07-22 00:10:13 +0200

    [ NEW ] : add value to enum

--- model/src/main/java/io/nbelleme/hvsz/game/GameDifficulty.java
+++ model/src/main/java/io/nbelleme/hvsz/game/GameDifficulty.java
@@ -1,7 +1,17 @@
 package io.nbelleme.hvsz.game;
 
-public enum  GameDifficulty {
-  EASY,
-  NORMAL,
-  HARD
+public enum GameDifficulty {
+  EASY(0.33),
+  NORMAL(0.5),
+  HARD(0.66);
+
+  private double value;
+
+  GameDifficulty(double value) {
+    this.value = value;
+  }
+
+  public double getValue() {
+    return value;
+  }
 }
