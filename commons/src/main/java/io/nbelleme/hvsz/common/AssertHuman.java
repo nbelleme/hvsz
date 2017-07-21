c2fbe969291d3c84b9f710b9412f88b452b70792
nbelleme <nbelleme@e-biz.fr>
2017-07-22 00:40:49 +0200

    [ NEW ] : create assert for human

---
+++ commons/src/main/java/io/nbelleme/hvsz/common/AssertHuman.java
@@ -0,0 +1,29 @@
+package io.nbelleme.hvsz.common;
+
+import io.nbelleme.hvsz.common.exceptions.HumanIsDeadException;
+import io.nbelleme.hvsz.humans.Human;
+
+public abstract class AssertHuman {
+  /**
+   * Asserts that a human is not null and alive.
+   *
+   * @param human the human life
+   */
+  public static void humanAlive(Human human) {
+    if (human == null || !human.isAlive()) {
+      throw new HumanIsDeadException();
+    }
+  }
+
+  /**
+   * Asserts that a human is not null and alive.
+   *
+   * @param human the human life
+   * @param zone  zone that asked the check
+   */
+  public static void humanAlive(Human human, Object zone) {
+    if (human == null || !human.isAlive()) {
+      throw new HumanIsDeadException(zone);
+    }
+  }
+}
