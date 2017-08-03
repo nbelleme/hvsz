9bb9bd0dccd04fb8899373ad707ac033fd0a0ba0
nbelleme <nbelleme-ext@apec.fr>
2017-08-03 14:16:42 +0200

    [ NEW ] : add nogamedefined exception handler

--- webservices/src/main/java/io/nbelleme/hvsz/controller/impl/ExceptionHandlerController.java
+++ webservices/src/main/java/io/nbelleme/hvsz/controller/impl/ExceptionHandlerController.java
@@ -1,11 +1,20 @@
 package io.nbelleme.hvsz.controller.impl;
 
+import io.nbelleme.hvsz.common.exceptions.NoGameDefinedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Created by nbelleme on 03/08/2017.
  */
 @ControllerAdvice
 final class ExceptionHandlerController {
+
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NoGameDefinedException.class)
+  public void handleNoGameDefinedException() {
+  }
 
 }
