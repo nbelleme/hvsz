24e3db1338b2c4672ee4fa5d2e64ef1ca809939f
nbelleme <nbelleme-ext@apec.fr>
2017-08-03 14:15:59 +0200

    [ NEW ] : add exception handler controller

---
+++ webservices/src/main/java/io/nbelleme/hvsz/controller/impl/ExceptionHandlerController.java
@@ -0,0 +1,11 @@
+package io.nbelleme.hvsz.controller.impl;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by nbelleme on 03/08/2017.
+ */
+@ControllerAdvice
+final class ExceptionHandlerController {
+
+}
