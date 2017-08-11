69dccfbfc6904ba40aa432d3e14917f58885e1ee
nbelleme <nbelleme@e-biz.fr>
2017-08-11 15:39:32 +0200

    [ DLT ] : remove default configuration files dozer

--- webservices/src/main/java/io/nbelleme/hvsz/config/DozerConfig.java
+++ webservices/src/main/java/io/nbelleme/hvsz/config/DozerConfig.java
@@ -4,8 +4,6 @@ import org.dozer.DozerBeanMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class DozerConfig {
@@ -17,14 +15,6 @@ public class DozerConfig {
    */
   @Bean(name = "org.dozer.Mapper")
   public DozerBeanMapper dozerBean() {
-    List<String> mappingFiles = Arrays.asList(
-        "dozer-global-configuration.xml",
-        "dozer-bean-mappings.xml",
-        "more-dozer-bean-mappings.xml"
-    );
-
-    DozerBeanMapper dozerBean = new DozerBeanMapper();
-    dozerBean.setMappingFiles(mappingFiles);
-    return dozerBean;
+    return new DozerBeanMapper();
   }
 }
