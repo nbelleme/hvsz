1c5f74ac8aeae5e014d473ef2bf570d61da2de9b
nbelleme <nbelleme@e-biz.fr>
2017-08-11 17:22:46 +0200

    [ NEW ] : activate CORS

--- webservices/src/main/java/io/nbelleme/hvsz/WebappApplication.java
+++ webservices/src/main/java/io/nbelleme/hvsz/WebappApplication.java
@@ -7,14 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableScheduling
 @ComponentScan(basePackages = {"io.nbelleme.*"})
-
-public class WebappApplication {
+public class WebappApplication extends WebMvcConfigurerAdapter{
 
   /**
    * Webapp application.
@@ -23,6 +25,11 @@ public class WebappApplication {
    */
   public static void main(String[] args) {
     SpringApplication.run(WebappApplication.class, args);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
   }
 
   /**
